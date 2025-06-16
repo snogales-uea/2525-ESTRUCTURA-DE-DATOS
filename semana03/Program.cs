7f9104b748852123e976528fbb20a59ce70d0ee8
santyng <s.nogales@outlook.com>
2025-06-16 08:42:32 -0500

    semana03
    semana03

---
+++ semana03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("2525 - ESTRUCTURA DE DATOS - UEA / SEMANA 03");
+Console.WriteLine("\n");
+
+string[] ltelefono = ["0991111111", "0992222222", "0993333333"];
+Estudiante estudiante = new Estudiante(1, "Carlos", "Torres", "Puyo, Av. Principal 123", ltelefono);
+
+//estudiante.Saludar();
+estudiante.MostrarInformacion();
